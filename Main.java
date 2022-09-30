06b65846745ed8557dc343249eb37ead9300cdc2
sule0 <99013587+sule0@users.noreply.github.com>
2022-09-30 12:33:19 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,11 +6,11 @@ public class Main {
 		int sayi2=34;
 		int sayi3=12;
 		if ((sayi<sayi2) && (sayi<sayi3) ) {
-			System.out.println(sayi +" smaller than another number");}
+			System.out.println(sayi +" smaller than other number");}
 		else if ((sayi2<sayi) && (sayi2<sayi3) ) {
-				System.out.println(sayi2 +" smaller than another number");}
+				System.out.println(sayi2 +" smaller than other number");}
 		else if ((sayi3<sayi2) && (sayi3<sayi) ) {
-					System.out.println(sayi3 +" smaller than another number");	
+					System.out.println(sayi3 +" smaller than other number");	
 		}
 		
 	}
