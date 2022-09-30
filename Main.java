442dd0e53901e18bcf35027c7c194e39d6d4f79c
sule0 <99013587+sule0@users.noreply.github.com>
2022-09-30 12:29:16 +0300

    find-small-number

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi=24;
+		int sayi2=34;
+		int sayi3=12;
+		if ((sayi<sayi2) && (sayi<sayi3) ) {
+			System.out.println(sayi +" smaller than another number");}
+		else if ((sayi2<sayi) && (sayi2<sayi3) ) {
+				System.out.println(sayi2 +" smaller than another number");}
+		else if ((sayi3<sayi2) && (sayi3<sayi) ) {
+					System.out.println(sayi3 +" smaller than another number");	
+		}
+		
+	}
+
+}
